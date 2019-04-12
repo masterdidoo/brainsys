95da49f34a8c3d97f904aaeae749dbb5ae6d3b47
Alexandr <975396+masterdidoo@users.noreply.github.com>
2019-04-13 01:48:44 +0400

    to ints

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
 #include <Arduino.h>
-#include "TM1637.h"
+#include <U8g2lib.h>
+//#include <Wire.h>
 
 #define RESET 6
 #define START 7
-#define CLK 8
-#define DIO 9
+
 #define BEEP 10
 
 #define ALL_KEYS 0b11111100
@@ -16,10 +16,19 @@ enum State
     state_read, state_time, state_answer, state_false, state_end
 };
 
-TM1637 display(CLK,DIO);
-//TM1637Display display(CLK, DIO);
-State state;
 
+State state;
+unsigned long start;
+uint8_t left_time;
+
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 void setup(void)
 {
     //set D2-7
@@ -32,9 +41,15 @@ void setup(void)
     // pinMode(RESET, INPUT_PULLUP);
     // pinMode(START, INPUT_PULLUP);
 
-    pinMode(BEEP, OUTPUT);
+    for(auto i=2;i<=7;i++){
+        pciSetup(i);
+    }
 
-    Serial.begin(9600);
+    pinMode(BEEP, OUTPUT);
+    pinMode(13, OUTPUT);
+
+    u8g2.begin();
+    //Serial.begin(9600);
 }
 
 uint8_t buttons;
@@ -70,11 +85,27 @@ uint8_t bitToTeam(void){
 }
 
 void displayTeam(uint8_t team){
-    display.display(1, team);
+    static char m_str[3+4];
+    strcpy(m_str, u8x8_u8toa(team, 2));
+    if (state != state_false){
+        m_str[2] = ' ';
+        auto cur = millis();
+        uint16_t ms = cur - start;
+        strcpy(m_str+3, u8x8_u16toa(ms, 3));
+    }
+    else{
+        strcpy(m_str+2, " fal");
+    }
+
+    u8g2.firstPage();
+    do {
+        u8g2.setFont(u8g2_font_logisoso32_tn);
+        u8g2.drawStr(0,32,m_str);
+    } while ( u8g2.nextPage() );
 }
 
 void readMainPins(void){
-    buttons = PINC & enabled;
+    buttons = (~PIND) & enabled;
     if (!buttons) return;
 
     if (state == state_read){
@@ -95,8 +126,6 @@ void readMainPins(void){
     }
 }
 
-unsigned long start;
-uint8_t left_time;
 
 void updateTimer(void){
     if (state != state_time){
@@ -107,8 +136,13 @@ void updateTimer(void){
         start += 1000;
         left_time--;
         
-        display.display(2, left_time / 10);//??
-        display.display(3, left_time % 10);//??
+        static char m_str[3];
+        strcpy(m_str, u8x8_u8toa(left_time, 2));
+        u8g2.firstPage();
+        do {
+            u8g2.setFont(u8g2_font_logisoso32_tn);
+            u8g2.drawStr(64,32,m_str);
+        } while ( u8g2.nextPage() );
 
         if (left_time <= 0){
             timeOutTone();
@@ -123,29 +157,55 @@ void updateTimer(void){
 }
 
 void readKeys(void){
-    if (digitalRead(RESET)) {
-        if (state != state_false) enabled = TEAM_KEYS;
+    if (!digitalRead(RESET)) {
+        if (state != state_false) {
+            enabled = TEAM_KEYS;
+        }
         state = state_read;
         left_time = 0;
-        display.clearDisplay();
+        u8g2.clear();
+        auto ms = millis();
+        while((millis() - ms) < 100) {
+            if (!digitalRead(RESET)) ms = millis();
+            yield();
+        }
+        // u8g2.firstPage();
+        // do {
+        //     // u8g2.drawLine(0,0,32,32);
+        //     // u8g2.drawLine(32,0,0,32);
+        // } while ( u8g2.nextPage() );
     }
     if (state != state_read && state != state_answer) return;
-    if (digitalRead(START)) {
-        state = state_time;
-        start = micros() - 1000000;
+    if (!digitalRead(START)) {
         left_time = state == state_read ? 61 : 21;
+        state = state_time;
+        start = millis() - 1000;
+
+        auto ms = millis();
+        while((millis() - ms) < 100) {
+            if (!digitalRead(RESET)) ms = millis();
+            yield();
+        }
+//        Serial.println("start");
     }
 }
 
 void loop(void)
 {
+    // auto ms = millis();
     readMainPins();
     readKeys();
     updateTimer();
+    // ms = millis() - ms;
+    // if (ms > 2) Serial.println(ms);
+    //delay(1000);
 }
 
+auto x = HIGH;
+
 // handle pin change interrupt for D0 to D7 here
-// ISR (PCINT2_vect)
-// {
-//     digitalWrite(13,digitalRead(7));
-// }
+ISR (PCINT2_vect)
+{
+    x = !x;
+    digitalWrite(13, x);
+}
